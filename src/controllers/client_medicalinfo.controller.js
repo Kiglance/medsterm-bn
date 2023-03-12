fc8d458405b93b796f995cb2076ae1a24cd2bf7b
gerard1001 <rutayisiregerard2002@gmail.com>
2023-03-12 03:29:19 +0200

    User med info assign

--- src/controllers/client_medicalinfo.controller.js
+++ src/controllers/client_medicalinfo.controller.js
@@ -80,66 +80,121 @@ export default class ClientMedicalInfoController {
           client_id: req.body.client_id
         }
       });
+
       if (!existingClient) {
         return res.status(404).json({
           error: "The Client you are trying assign doesn't exist."
         });
       }
 
-      const myTuts = req.body.info_id.split(', ');
+      
+      const str = req.body.info_id;
 
-      var checker = [];
-      var makeup = [];
-      for (let i = 0; i < myTuts.length; i++) {
-        const existingInfo = await Medical_Info.findOne({
-          where: {
-            info_id: myTuts[i]
+      if (str !== '' && str !== undefined) {
+        const new_arr = str.split('#&#&');
+
+        function getAnyt(arr) {
+          let newArr = [];
+          for (let i = 0; i < arr.length; i++) {
+            const obj = {};
+            obj['info_id'] = arr[i].split('#%#%')[0];
+            obj['description'] = arr[i].split('#%#%')[1];
+            newArr.push(obj);
           }
-        });
-
-        const existingRelation = await Client_MedicalInfo.findOne({
-          where: { client_id: req.body.client_id, info_id: myTuts[i] }
-        }).catch((err) => {});
-        if (!existingRelation && existingInfo) {
-          makeup.push(i);
-        }
-        checker.push(existingRelation);
-      }
-
-      if (makeup == '') {
-        return res.status(404).send({
-          error: 'All these relations have already been made.'
-        });
-      }
-
-      const getBulkArr = (myTuts) => {
-        var arr = [];
-        var newArr = [];
-        for (let i = 0; i < myTuts.length; i++) {
-          var obj = {};
-          obj['client_id'] = req.body.client_id;
-          obj['info_id'] = myTuts[i];
-
-          arr.push(obj);
+          return newArr;
         }
 
-        for (let i = 0; i < arr.length; i++) {
-          if (makeup.includes(i)) {
-            newArr.push(arr[i]);
+        const first_arr = getAnyt(new_arr);
+        let final_arr = [];
+
+        for (let i = 0; i < first_arr.length; i++) {
+          let obj = {};
+          obj['client_id'] = newUser.client_id;
+          obj['info_id'] = first_arr[i].info_id;
+          obj['description'] = first_arr[i].description;
+          const id = first_arr[i].info_id;
+          const existingInfo =
+            await this.medicalInfoService.getSingleMedicalInfo(id);
+          if (existingInfo) {
+            final_arr.push(obj);
           }
         }
-        return newArr;
-      };
 
-      await this.clientMedicalInfoService.createClientMedicalInfoArr(
-        getBulkArr(myTuts),
-        res
-      );
+        if (final_arr == '') {
+          return res.status(500).json({
+            message: 'Final array is empty'
+          });
+        }
 
-      return res.status(201).send({
-        message: "Medical info added to patient'n data successfully.",
-        data
-      });
+        const newInfo =
+          await this.clientMedicalInfoService.createClientMedicalInfo(
+            final_arr,
+            res
+          );
+
+          return res.status(201).json({
+            status: 201,
+            message:
+              'Medical info added to patient\'n data successfully..'
+           
+          });
+      }
+
+
+      // const myTuts = req.body.info_id.split(', ');
+
+      // var checker = [];
+      // var makeup = [];
+      // for (let i = 0; i < myTuts.length; i++) {
+      //   const existingInfo = await Medical_Info.findOne({
+      //     where: {
+      //       info_id: myTuts[i]
+      //     }
+      //   });
+
+      //   const existingRelation = await Client_MedicalInfo.findOne({
+      //     where: { client_id: req.body.client_id, info_id: myTuts[i] }
+      //   }).catch((err) => {});
+      //   if (!existingRelation && existingInfo) {
+      //     makeup.push(i);
+      //   }
+      //   checker.push(existingRelation);
+      // }
+
+      // if (makeup == '') {
+      //   return res.status(404).send({
+      //     error: 'All these relations have already been made.'
+      //   });
+      // }
+
+      // const getBulkArr = (myTuts) => {
+      //   var arr = [];
+      //   var newArr = [];
+      //   for (let i = 0; i < myTuts.length; i++) {
+      //     var obj = {};
+      //     obj['client_id'] = req.body.client_id;
+      //     obj['info_id'] = myTuts[i];
+
+      //     arr.push(obj);
+      //   }
+
+      //   for (let i = 0; i < arr.length; i++) {
+      //     if (makeup.includes(i)) {
+      //       newArr.push(arr[i]);
+      //     }
+      //   }
+      //   return newArr;
+      // };
+
+      // await this.clientMedicalInfoService.createClientMedicalInfoArr(
+      //   getBulkArr(myTuts),
+      //   res
+      // );
+
+      // return res.status(201).send({
+      //   message: "Medical info added to patient'n data successfully.",
+      //   data
+      // });
 
       // await Client_MedicalInfo.bulkCreate(getBulkArr(myTuts))
       //   .then((data) => {
@@ -161,4 +216,93 @@ export default class ClientMedicalInfoController {
       });
     }
   }
+
+  // async assignInfo(req, res) {
+  //   try {
+  //     const existingClient = await Client.findOne({
+  //       where: {
+  //         client_id: req.body.client_id
+  //       }
+  //     });
+  //     if (!existingClient) {
+  //       return res.status(404).json({
+  //         error: "The Client you are trying assign doesn't exist."
+  //       });
+  //     }
+
+  //     const myTuts = req.body.info_id.split(', ');
+
+  //     var checker = [];
+  //     var makeup = [];
+  //     for (let i = 0; i < myTuts.length; i++) {
+  //       const existingInfo = await Medical_Info.findOne({
+  //         where: {
+  //           info_id: myTuts[i]
+  //         }
+  //       });
+
+  //       const existingRelation = await Client_MedicalInfo.findOne({
+  //         where: { client_id: req.body.client_id, info_id: myTuts[i] }
+  //       }).catch((err) => {});
+  //       if (!existingRelation && existingInfo) {
+  //         makeup.push(i);
+  //       }
+  //       checker.push(existingRelation);
+  //     }
+
+  //     if (makeup == '') {
+  //       return res.status(404).send({
+  //         error: 'All these relations have already been made.'
+  //       });
+  //     }
+
+  //     const getBulkArr = (myTuts) => {
+  //       var arr = [];
+  //       var newArr = [];
+  //       for (let i = 0; i < myTuts.length; i++) {
+  //         var obj = {};
+  //         obj['client_id'] = req.body.client_id;
+  //         obj['info_id'] = myTuts[i];
+
+  //         arr.push(obj);
+  //       }
+
+  //       for (let i = 0; i < arr.length; i++) {
+  //         if (makeup.includes(i)) {
+  //           newArr.push(arr[i]);
+  //         }
+  //       }
+  //       return newArr;
+  //     };
+
+  //     await this.clientMedicalInfoService.createClientMedicalInfoArr(
+  //       getBulkArr(myTuts),
+  //       res
+  //     );
+
+  //     return res.status(201).send({
+  //       message: "Medical info added to patient'n data successfully.",
+  //       data
+  //     });
+
+  //     // await Client_MedicalInfo.bulkCreate(getBulkArr(myTuts))
+  //     //   .then((data) => {
+  //     //     return res.status(200).send({
+  //     //       message: "Medical info added to patient'n data successfully.",
+  //     //       data
+  //     //     });
+  //     //   })
+  //     //   .catch((error) => {
+  //     //     return res.status(500).json({
+  //     //       message: 'Error occured while creating med. infos',
+  //     //       error: error.message
+  //     //     });
+  //     //   });
+  //   } catch (error) {
+  //     return res.status(500).json({
+  //       message: 'Error occured while creating med. infos',
+  //       error: error.message
+  //     });
+  //   }
+  // }
 }
